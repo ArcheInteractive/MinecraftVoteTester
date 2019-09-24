8cf4dde26180a058dcd604835451405c8504d67e
Evan Lindsay <evanlindsay@live.com>
2019-09-23 21:35:27 -0700

    Update build scripts

--- src/main/java/com/archeinteractive/minecraft/vote_tester/Main.java
+++ src/main/java/com/archeinteractive/minecraft/vote_tester/Main.java
@@ -20,6 +20,10 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import com.archeinteractive.minecraft.vote_tester.protocol.HandshakeHandler;
 
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 import java.util.regex.Pattern;
 
 public class Main extends Application {
@@ -42,9 +46,27 @@ public class Main extends Application {
     public TextField tokenField = new TextField();
 
     public static void main(String[] args) {
-        eventLoopGroup = new NioEventLoopGroup(1);
-        launch(args);
-        eventLoopGroup.shutdownGracefully();
+        Logger logger = Logger.getLogger("VoteTester");
+        FileHandler fh;
+
+        try {
+            fh = new FileHandler("./application.log");
+            logger.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        logger.info("Starting...");
+
+        try {
+            eventLoopGroup = new NioEventLoopGroup(1);
+            launch(args);
+            eventLoopGroup.shutdownGracefully();
+        } catch (Exception ex) {
+            logger.log(Level.SEVERE, "An exception was caught: ",ex);
+        }
     }
 
     @Override
